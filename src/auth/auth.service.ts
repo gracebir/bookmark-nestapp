2dbaaa66bcd8f908ac8e5ef84c968b2c1e75742a
gracebir <grace.devolop@gmail.com>
2024-07-07 19:32:04 +0200

    creating modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: 'I am sign up' };
+  }
+
+  signin() {
+    return { msg: 'I am sign in' };
+  }
+}
